c2db2e50263c3c55db10d08ddae3d0b12b76ca88
Ali-Sid <59683051+Ali-Sid@users.noreply.github.com>
2023-05-31 17:01:38 +0530

    upload file

---
+++ day15/exercise.js
@@ -0,0 +1,50 @@
+// 30-days-react-challenge: Day 15: Third Party Packages
+// Exercises: Level 1
+
+// 1. What is a package?
+// A package is a collection of code files and resources bundled together for distribution and reuse.
+// It helps organize and manage code by grouping related functionalities.
+// Packages are commonly used in software development to modularize code, promote code reuse, and leverage community contributions.
+// They can be distributed through package managers and often include metadata for managing dependencies and versioning.
+// Using packages saves time, promotes modularity, and enables collaboration.
+
+// 2. What is a third party package?
+// A third-party package is software developed by individuals or organizations outside the core development team.
+// It extends the functionality of a software or platform and provides additional features.
+// These packages are commonly used in web development to add specific functionalities to applications or websites.
+// They save development time and offer ready-to-use solutions.
+
+// 3. Do you have to use third party packages?
+// It ultimately depends on the specific requirements of my project and whether third-party packages can provide the desired functionality and productivity benefits.
+// Many applications and systems can be built entirely using native tools and libraries provided by the programming language or framework.
+// However, third-party packages can significantly enhance development by providing pre-built solutions, saving time and effort.
+// They offer ready-made functionalities, libraries, and tools that can help with common tasks, such as handling data, user interfaces, authentication, and more.
+
+// 4. How do you know the popularity and stability of a third party package?
+// By looking at the no. of downloads in the npm website, github stars, community support, version history, license and open source, by reading reviews and feedback from other developers etc..
+
+// 5. How many JavaScript packages are there on the npm registry?
+// There are over 1 million JavaScript oackages on the npm registry.
+
+// 6. How do you install a third party package?
+// By using a package manager either npm or yarn
+// ex: npm install [package-name] / npm i [package-name]
+
+// 7. What packages do you use most frequently?
+// I use several different packages depending upon the projects I build.
+// Some of them are bootstrap for react, axios for making API requests, styled components, etc..
+
+// 8. What package do you use to fetch data?
+// We use axios package to fetch data.
+// Axios can be used to send HTTP requests to a server and handle the responses, making it suitable for fetching data from APIs. It supports different types of requests like GET, POST, PUT, DELETE, etc. It also allows setting headers, handling authentication, and handling response data in various formats (JSON, FormData, etc.).
+
+
+// 9. What is the purpose of classnames package?
+// The purpose of the classnames package is to simplify the process of conditionally applying CSS classes to elements in React applications. It provides a convenient way to generate class names based on different conditions or dynamic values.
+
+// In React, applying conditional CSS classes is commonly done by using JavaScript expressions within the className attribute. However, when dealing with multiple conditions or dynamic class names, the code can become verbose and hard to manage. The classnames package helps alleviate this issue by providing a utility function that combines and resolves class names based on different inputs.
+
+
+// 10. What is the pupose validator package?
+// The purpose of a validator package, such as validator or yup, is to provide a set of utility functions or tools to validate data in JavaScript or TypeScript applications. These packages offer a wide range of validation methods that help ensure the correctness, integrity, and consistency of user input or data throughout the application.
+// Validator packages are valuable tools for ensuring the validity and consistency of data in JavaScript or TypeScript applications. They help reduce manual validation code, provide reusable validation functions, and enhance the robustness and reliability of your application's data processing and user interactions.
